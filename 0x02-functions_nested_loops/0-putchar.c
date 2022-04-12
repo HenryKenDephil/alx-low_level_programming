719fafab8f64835e2e46a021a6379257935237a4
HenryKenDephil <h.komoloh@gmail.com>
2022-04-12 20:38:50 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * main-main body
+ * -putchar writes the character c on stdout 
+ * Return on success 0
+ */
+int -putchar(char c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
