f0b5ceb8bbc9f8c0225c2ba7d111d10388dac4be
HenryKenDephil <h.komoloh@gmail.com>
2022-11-02 19:41:00 +0300

    updating codes

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,28 +10,34 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-    int max = (size -1);   //the value at the last index of the array
-    int min = 0;   //the value at index zero
-    int mid;      //mid holds the index of middle value or the first guess element position
-    int i = 0;   //i finds out the index position of the value after complete search
+    size_t max = (size -1);   /*the value at the last index of the array*/
+    size_t min = 0;   /*the value at index zero*/
+    size_t mid;     /*mid holds the index of middle value or the first guess element position*/
+    size_t i = 0;   /*i finds out the index position of the value after complete search*/
+
+    if(array == NULL)
+        return -1;
 
     while(max >= min)
     {
-        int mid = floor(max + min)/2;
-        i++; //increamenting the index from zero by 1 step after the other
+         mid = floor(max + min)/2;
+        printf("Searching in array: ");
+        
+        for (i = min; i <= max; i++)
+			i < max ? printf("%d, ", array[i]) : printf("%d\n", array[i]);
+
         if(array[mid] == value)
         {
-            printf("index for the value: %d\n", i);
             return mid;
         }
         else if(array[mid] > value)
         {
-            max = (mid - 1); // targeted value would be in the left half
+            max = (mid - 1); /*targeted value would be in the left half*/
         }
         else
         {
-            min = (mid + 1); //targeted value is found in the right half
+            min = (mid + 1); /*targeted value is found in the right half*/
         }
     }
-    return -1; //returns not found if element isn't in the array
+    return -1; /*returns not found if element isn't in the array*/
 }
